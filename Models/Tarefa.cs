4e456947e13c388a3e3793abf253f0d3ad5596d9
fabriciorosanet <161895481+fabriciorosanet@users.noreply.github.com>
2024-08-21 12:28:45 -0300

    Projeto finalizado - já estou utilizando com minhas anotações diarias

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -14,16 +14,16 @@ namespace ListaDeTarefasProjetoFiap.Models
         [Required(ErrorMessage = "Preencha a data de vencimento!")]
         public DateTime? DataDeVencimento { get; set; }
 
+        [Required(ErrorMessage = "Selecione uma categoria!")]
         public string CategoriaId { get; set; }
 
         [ValidateNever]
         public Categoria Categoria { get; set; }
 
+        [Required(ErrorMessage = "Selecione um status!")]
         public string StatusId { get; set; }
-
         [ValidateNever]
         public Status Status { get; set; }
-
         public bool Atrasado => StatusId == "aberto" && DataDeVencimento < DateTime.Today;
     }
 }
